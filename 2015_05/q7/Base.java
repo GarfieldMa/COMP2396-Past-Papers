868f6cc5d41609ac5d3f15eaa4f90417beff632e
GarfieldMa <garfie1d@connect.hku.hk>
2016-12-06 15:26:49 +0800

    complete q7

---
+++ 2015_05/q7/Base.java
@@ -0,0 +1,6 @@
+public class Base{
+	int i = 100;
+	Base(){
+		System.out.println( i );
+	}
+}
---
+++ 2015_05/q7/Pri.java
@@ -0,0 +1,7 @@
+public class Pri extends Base{
+	static int i = 200;
+	public static void main(String[] args) {
+		Pri p = new Pri();
+		System.out.println( i );
+	}
+}
