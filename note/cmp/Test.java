32ffa56ad19b80cc2db848f7ed6051e372228580
GarfieldMa <garfie1d@connect.hku.hk>
2016-12-07 23:07:49 +0800

    git add .

---
+++ note/cmp/Test.java
@@ -0,0 +1,21 @@
+
+public class Test {
+	
+	// Constructor
+	public Test(){
+		
+	}
+
+	public static void main(String[] args){
+		String a = new String("Hello");
+		String b = "Hello";
+		
+		if ( a == "Hello" ){
+			System.out.println( "meo~" );
+		}
+		else{
+			System.out.println( a );
+		}
+	}
+	
+}
